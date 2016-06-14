2ddbb309791e7a270d38658b8b5ddcc09fd5749e
Filipe lemos <contato@filipelemos.com>
2016-06-14 01:17:12 -0300

    Adding gulp-sourcemaps support

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,22 @@
 var gulp = require("gulp");
+var sourceMaps = require("gulp-sourcemaps");
 var stylus = require("gulp-stylus");
+
+var distPaths = {
+  root: "./dist/"
+};
+
+var sourcePaths = {
+  css: "./src/stylus/**/*.styl",
+  stylus: "./src/stylus/main.styl"
+};
+
+gulp.task("css", function() {
+  return gulp.src(sourcePaths.stylus)
+    .pipe(sourceMaps.init())
+      .pipe(stylus({
+        compress: true
+      }))
+    .pipe(sourceMaps.write("."))
+    .pipe(gulp.dest(distPaths.root));
+});
