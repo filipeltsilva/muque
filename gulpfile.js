cbd10ff6901b054f345e215546726dc4c4ba4526
Filipe lemos <contato@filipelemos.com>
2016-06-20 18:40:46 -0300

    Improving the gulpfile.js file with watch and js tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,44 @@
 var gulp = require("gulp");
+var rename = require("gulp-rename");
 var sourceMaps = require("gulp-sourcemaps");
 var stylus = require("gulp-stylus");
+var uglify = require("gulp-uglify");
 
-var distPaths = {
+var distPath = {
   root: "./dist/"
 };
 
-var sourcePaths = {
+var sourcePath = {
   css: "./src/stylus/**/*.styl",
+  javascript: "./src/javascripts/**/*.js",
   stylus: "./src/stylus/main.styl"
 };
 
+function addMinifiedFileSuffix(renameTask) {
+  return renameTask({
+    suffix: ".min"
+  });
+}
+
 gulp.task("css", function() {
-  return gulp.src(sourcePaths.stylus)
+  return gulp.src(sourcePath.stylus)
     .pipe(sourceMaps.init())
       .pipe(stylus({
         compress: true
       }))
     .pipe(sourceMaps.write("."))
-    .pipe(gulp.dest(distPaths.root));
+    .pipe(addMinifiedFileSuffix(rename))
+    .pipe(gulp.dest(distPath.root));
+});
+
+gulp.task("js", function() {
+  return gulp.src(sourcePath.javascript)
+    .pipe(uglify())
+    .pipe(addMinifiedFileSuffix(rename))
+    .pipe(gulp.dest(distPath.root));
+});
+
+gulp.task("watch", function() {
+  gulp.watch(sourcePath.css, ["css"]);
+  gulp.watch(sourcePath.javascript, ["js"]);
 });
