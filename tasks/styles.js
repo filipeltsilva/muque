c26a364a5c748c7bb7e5b75dc5a08b6f96f7507b
Filipe Lemos <contato@filipelemos.com>
2017-02-01 18:15:41 -0200

    Adding PostCSS and Autoprefixer support

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const config     = require('../config');
-const cssMin     = require('gulp-cssmin');
-const gulp       = require('gulp');
-const koutoSwiss = require('kouto-swiss');
-const plumber    = require('gulp-plumber');
-const sourceMaps = require('gulp-sourcemaps');
-const stylus     = require('gulp-stylus');
+const autoPrefixer = require('autoprefixer');
+const config       = require('../config');
+const cssMin       = require('gulp-cssmin');
+const gulp         = require('gulp');
+const koutoSwiss   = require('kouto-swiss');
+const postCss      = require('gulp-postcss');
+const plumber      = require('gulp-plumber');
+const sourceMaps   = require('gulp-sourcemaps');
+const stylus       = require('gulp-stylus');
 
 function buildStyles() {
   return gulp.src(config.styles.mainFile)
@@ -15,6 +17,15 @@ function buildStyles() {
         .pipe(stylus({
           use: koutoSwiss()
         }))
+        .pipe(postCss([
+          autoPrefixer({
+            browsers: [
+              'last 3 versions',
+              'Android >= 4',
+              'IE >= 9'
+            ]
+          })
+        ]))
       .pipe(sourceMaps.write())
     .pipe(plumber.stop())
     .pipe(gulp.dest(config.distRoot));
